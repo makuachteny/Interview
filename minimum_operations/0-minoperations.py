92468f9a4c87c900b042c4b82f6179aca8bf7846
makuachteny <m.gatluak@alustudent.com>
2023-05-25 12:13:38 +0300

    Pep8 validation

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -8,7 +8,7 @@ Minimum Operations
 
 def minOperations(n):
     """
-    Calculates the fewest number of operations needed to result in exactly n 'H' characters in the file.
+    Calculates fewest number of operations needed
     """
     if n <= 1:
         return 0
