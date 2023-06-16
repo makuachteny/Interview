30458a4430cc725edb23ddbd9ac38b1ad742d0ef
makuachteny <m.gatluak@alustudent.com>
2023-06-16 15:02:55 +0300

    rain

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
-"""Calculating square units of water retained"""
+"""Algorithm for calculating retained rain water"""
 
-rain = __import__('0-rain').rain
 
-if __name__ == "__main__":
-    walls = [0, 1, 0, 2, 0, 3, 0, 4]
-    print(rain(walls))
-    walls = [2, 0, 0, 4, 0, 0, 1, 0]
-    print(rain(walls))
+def rain(walls):
+    if not walls or len(walls) < 3:
+        return 0
+    water = 0
+    for i in range(1, len(walls) - 1):
+        left = walls[i]
+        for j in range(i):
+            left = max(left, walls[j])
+        right = walls[i]
+        for j in range(i + 1, len(walls)):
+            right = max(right, walls[j])
+        water += min(left, right) - walls[i]
+    return water
