a98efdc61d69e871e2320f92f24864b80ad05554
Your Makuachx <teny.makuach@tufts.edu>
2025-10-20 21:20:44 -0400

    isPalindrome

---
+++ DS_&_Algorithms/4.isPalindrome.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0:
+            return False
+
+        x_str = str(x)
+        # Compare string with its
+        if x_str == x_str[::-1]:
+            return True
